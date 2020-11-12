defff21f8cd2d688b852c2b5c6f186eda9030527
Ioannis Margaris <johnmargaris90@gmail.com>
2020-11-12 17:05:04 +0000

    Feature: Updated Server to work more like chat
    - Instead of responding by sending back the buffer,
    - it now waits for server to send a message making this more like a chat

--- server.cpp
+++ server.cpp
@@ -19,19 +19,26 @@ int main()
         return -1;
     }
 
-    // Bind the socket to an ip address and port number
+    /* Setup the host_addr structure for use in bind call server byte order */
     sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(54000); //host to network short conversion
-    inet_pton(AF_INET, "0.0.0.0", &serv_addr.sin_addr); // This is our binding information 
+    // Convert short integer value for port must be converted into network byte order
+    serv_addr.sin_port = htons(54000); 
+    // Automatically be filled with current host's IP address
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
 
+    /* bind() passes file descriptor, the address structure, and the length of the address structure
+    This bind() call will bind  the socket to the current IP address on port */
     if (bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
     {
         perror("Error binding to IP/Port!\n");
         return -2;
     }
 
-    // Mark the socket for listening in
+    /* This listen() call tells the socket to listen to the incoming connections.
+    The listen() function places all incoming connection into a backlog queue until accept() call accepts the connection.
+    Here, we set the maximum size for the backlog queue to SOMAXCONN or 4096.*/
+
     if (listen(sockfd, SOMAXCONN) < 0)
     {
         perror("Error listening!\n");
@@ -45,6 +52,11 @@ int main()
     char svc[NI_MAXSERV]; // Service (i.e port) the client is connected on
 
     std::cout << "Waiting for a connection...\n";
+
+    /* This accept() function will write the connecting client's address info into the the address structure 
+    and the size of that structure is clilen. The accept() returns a new socket file descriptor for the accepted connection.
+    So, the original socket file descriptor can continue to be used for accepting new connections 
+    while the new socket file descriptor is used for communicating with the connected client. */
 
     int clientSocket = accept(sockfd, (sockaddr *)&client, &clientSize);
     if (clientSocket < 0)
@@ -66,10 +78,11 @@ int main()
         std::cout << host << " connected on port: " << ntohs(client.sin_port) << "!\n";
     }
     
-    // Close the first socket
+    // Close the original socket
     close(sockfd);
 
     char buff[4096];
+    std::string serv_response;
 
     // While receiving - Display message, echo message
     while (true)
@@ -94,8 +107,10 @@ int main()
         // Display the message received from the client 
         std::cout << "Message received: " << std::string(buff, 0, bytesReceived) << "\n";
 
+        std::cout << "> ";
+        getline(std::cin, serv_response);
         // Send the message back to the client
-        send(clientSocket, buff, bytesReceived + 1, 0);
+        send(clientSocket, serv_response.c_str(), serv_response.size()+ 1, 0);
     }
 
     // Close the socket
